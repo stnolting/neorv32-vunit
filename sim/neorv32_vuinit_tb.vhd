6a2d712832b028916064ecac4e0229c8a9acc02d
stnolting <stnolting@gmail.com>
2024-12-30 10:44:26 +0100

    [sim] update core generics

--- sim/neorv32_vuinit_tb.vhd
+++ sim/neorv32_vuinit_tb.vhd
@@ -193,7 +193,6 @@ begin
     -- Processor Clocking --
     CLOCK_FREQUENCY       => f_clock_c,
     -- Identification --
-    HART_ID               => x"00000000",
     JEDEC_ID              => "00000000000",
     -- Boot Configuration --
     BOOT_MODE_SELECT      => 1,
@@ -261,7 +260,7 @@ begin
     XIRQ_NUM_CH           => 32,
     -- Processor peripherals --
     IO_GPIO_NUM           => 64,
-    IO_MTIME_EN           => true,
+    IO_CLINT_EN           => true,
     IO_UART0_EN           => true,
     IO_UART0_RX_FIFO      => 32,
     IO_UART0_TX_FIFO      => 32,
